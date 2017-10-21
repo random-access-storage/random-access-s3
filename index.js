80e71048f2f5c2d87adae16231b5023e5e75da35
e-e-e <benjamin.mforster@gmail.com>
2017-10-21 16:30:43 +1100

    add option for setting s3 instance

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ var S3 = require('aws-sdk/clients/s3')
 var Abstract = require('abstract-random-access')
 var inherits = require('inherits')
 
-var s3 = new S3()
-
 var Store = function (filename, options) {
   if (!(this instanceof Store)) return new Store(filename, options)
   Abstract.call(this)
+  this.s3 = options.s3 || new S3()
   this.key = filename
   this.bucket = options.bucket
   inherits(Store, Abstract)
@@ -19,7 +18,7 @@ Store.prototype._read = function (offset, length, callback) {
     Key: this.key,
     Range: `bytes=${offset}-${offset + length}`
   }
-  s3.getObject(params, (err, data) => {
+  this.s3.getObject(params, (err, data) => {
     if (err) return callback(err)
     callback(null, data.Body)
   })
